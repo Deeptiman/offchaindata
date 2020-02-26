e5e8d28a4137a06887df749559e7fccfe5172833
Deeptiman Pattnaik <pattnaikdeeptiman@gmail.com>
2020-02-26 05:55:39 +0200

    Update model.go

--- model.go
+++ model.go
@@ -4,6 +4,6 @@ type SampleUser struct {
 
 	Email 	string 		`json:"email"`	
 	Name 	string 		`json:"name"`
-	Age		string		`json:"age"`
+	Age	string		`json:"age"`
 	Country string		`json:"country"`
 }
